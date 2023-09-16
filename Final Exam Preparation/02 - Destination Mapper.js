21ba133bcdbbdb9fc532f1b1cb11b4fb4abc7490
ivailospasov85 <133914017+ivailospasov85@users.noreply.github.com>
2023-09-16 21:43:35 +0300

    NEW

--- Final Exam Preparation/02 - Destination Mapper.js
+++ Final Exam Preparation/02 - Destination Mapper.js
@@ -16,10 +16,36 @@
 // Output
 // • Print the messages described above
 
-function destinationMapper(input){
+function destinationMapper(input) {
+    // let text = "=Hawai=/Cyprus/=Invalid/invalid==i5valid=/I5valid/=i="
+    let regex = /=[A-Z]*[A-Za-z+]{3,}=|\/[A-Z]*[A-Za-z+]{3,}\//gm
 
+    let result = []
+    let destinations = input.match(regex)
+
+    if (destinations !== null) {
+
+        for (let element of destinations) {
+
+            if (element.includes('=')) {
+                let newElement = element.substring(1, element.length - 1)
+                result.push(newElement)
+            } else if (element.includes('/')) {
+                let newElement = element.substring(1, element.length - 1)
+                result.push(newElement)
+            }
+
+        }
+    }
+    let n = result.join(', ')
+    let travelPoints = result.reduce((total, destination) => total + destination.length, 0);
+    if (travelPoints < 0) {
+        travelPoints = 0
+    }
+    console.log(`Destinations: ${n}`);
+    console.log(`Travel Points: ${travelPoints}`);
 
 
 }
 
-destinationMapper(("=Hawai=/Cyprus/=Invalid/invalid==i5valid=/I5valid/=i="))
+destinationMapper("=Hawai=/Cyprus/=Invalid/invalid==i5valid=/I5valid/=i=") 
